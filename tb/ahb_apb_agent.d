4448d3df87cdbfcc3849cfaec1fc06551690e0fb
Sachin Bisen <bisendsachin@gmail.com>
2026-02-17 17:34:17 +0530

    Updated Testbench Environment

--- tb/ahb_apb_agent.d
+++ tb/ahb_apb_agent.d
@@ -1,11 +1,12 @@
 import esdl;
 import uvm;
 import ahb_apb_monitor: ahb_apb_monitor;
+import ahb_apb_driver: ahb_apb_driver;
 
 class ahb_apb_agent: uvm_agent
 {
 
-
+	@UVM_BUILD ahb_apb_driver driver;
         @UVM_BUILD ahb_apb_monitor monitor;
 
 	mixin uvm_component_utils;
--- tb/ahb_apb_test.d
+++ tb/ahb_apb_test.d
@@ -3,6 +3,7 @@ import uvm;
 import ahb_apb_env: ahb_apb_env;
 import ahb_apb_agent: ahb_apb_agent;
 import ahb_apb_intf: ahb_apb_intf;
+import ahb_apb_slave: ahb_apb_slave;
 
 // AHB_APB DIRECTED TEST
 
@@ -41,7 +42,9 @@ class ahb_apb_tb_top : Entity
     import esdl.intf.verilator.verilated;
     import esdl.intf.verilator.trace;
 
-    ahb_apb_intf apbSlave;
+   // ahb_apb_intf apbSlave;   
+   // ahb_apb_intf ahbSlave;
+      ahb_apb_intf bus_if;
     VerilatedFstD _trace;
 
     Signal!(ubvec!1) H_CLK;
@@ -72,25 +75,25 @@ class ahb_apb_tb_top : Entity
     }
 
     override void doConnect() {
-        apbSlave.H_CLK(H_CLK);
-        apbSlave.H_RESET_n(H_RESET_n);
+        bus_if.H_CLK(H_CLK);
+        bus_if.H_RESET_n(H_RESET_n);
 
-        apbSlave.H_WDATA(dut.H_WDATA);
-	apbSlave.H_ADDR(dut.H_ADDR);
-	apbSlave.P_RDATA(dut.P_RDATA);
-	apbSlave.H_WRITE(dut.H_WRITE);
-	apbSlave.H_TRANS(dut.H_TRANS);
-	apbSlave.H_SEL_APB(dut.H_SEL_APB);
-	apbSlave.H_READY_IN(dut.H_READY_IN);
+        bus_if.H_WDATA(dut.H_WDATA);
+	bus_if.H_ADDR(dut.H_ADDR);
+	bus_if.P_RDATA(dut.P_RDATA);
+	bus_if.H_WRITE(dut.H_WRITE);
+	bus_if.H_TRANS(dut.H_TRANS);
+	bus_if.H_SEL_APB(dut.H_SEL_APB);
+	bus_if.H_READY_IN(dut.H_READY_IN);
 
-	apbSlave.P_WDATA(dut.P_WDATA);
-	apbSlave.H_RDATA(dut.H_RDATA);
-	apbSlave.P_ADDR(dut.P_ADDR);
-	apbSlave.P_ENABLE(dut.P_ENABLE);
-	apbSlave.P_WRITE(dut.P_WRITE);
-	apbSlave.H_RESP(dut.H_RESP);
-	apbSlave.P_SELx(dut.P_SELx);
-	apbSlave.H_READY_OUT(dut.H_READY_OUT);
+	bus_if.P_WDATA(dut.P_WDATA);
+	bus_if.H_RDATA(dut.H_RDATA);
+	bus_if.P_ADDR(dut.P_ADDR);
+	bus_if.P_ENABLE(dut.P_ENABLE);
+	bus_if.P_WRITE(dut.P_WRITE);
+	bus_if.H_RESP(dut.H_RESP);
+	bus_if.P_SELx(dut.P_SELx);
+	bus_if.H_READY_OUT(dut.H_READY_OUT);
     }
 
     // Clock + Reset tasks
@@ -132,11 +135,13 @@ class ahb_apb_tb_top : Entity
 class ahb_apb_tb : uvm_context
 {
     ahb_apb_tb_top top;
-
+    import ahb_apb_test;
     override void initial() {
         // Pass interface to driver using config DB
         uvm_config_db!(ahb_apb_intf).set(null,
-            "uvm_test_top.env.agent.*", "ahb_if", top.apbSlave);
+            "uvm_test_top.env.agent.*", "ahb_if", top.bus_if);
+ 	uvm_config_db!(ahb_apb_intf).set(null,
+            "uvm_test_top.env.slave", "apb_if", top.bus_if);
     }
 }
 
